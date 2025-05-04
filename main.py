920d52ba619565d24419d56aae20b7b6254ef2de
TharindiW <158560964+TharindiWimalajeewa@users.noreply.github.com>
2025-05-04 09:29:18 +0530

    main() commit 1

--- main.py
+++ main.py
@@ -72,4 +72,27 @@ def add():
 
     CSV.add_entry (date,amount,category,description)
 
+def main():
+    while True:
+        print("\n1/ Add a new Transaction")
+        print("2. View summary within a date range")
+        print("3. Exit")
+        choice = input("Enter your choice (1-3)")
+
+        if choice =="1":
+            add()
+        elif choice == "2":
+            start_date = get_date("Enter the start date (dd-mm-yyyy)")
+            end_date = get_date("Enter the end date (dd-mm-yyyy)")
+            df = CSV.get_transactions(start_date,end_date)
+        elif choice == "3":
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice")
+
+if __name__ == "__main":
+    main()
+    
+
 CSV.get_transactions("01-01-2024","01-01-2026")
